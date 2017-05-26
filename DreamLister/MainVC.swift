75746c8b32c41606915b1a12a3ac55486a18be88
Jeff Ripke <jripke74@gmail.com>
2017-05-26 07:14:48 -0700

    add uiview and created interface

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  DreamLister
 //
 //  Created by Jeff Ripke on 5/25/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
