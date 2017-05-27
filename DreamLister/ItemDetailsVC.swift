70e38eb2f70902527e3ccade1acf21920c31334c
Jeff Ripke <jripke74@gmail.com>
2017-05-27 07:22:31 -0700

    connected fields in detailVC and add stores

--- DreamLister/ItemDetailsVC.swift
+++ DreamLister/ItemDetailsVC.swift
@@ -7,12 +7,66 @@
 //
 
 import UIKit
+import CoreData
 
-class ItemDetailsVC: UIViewController {
+class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var storePicker: UIPickerView!
+    @IBOutlet weak var titleField: CustomTextField!
+    @IBOutlet weak var priceField: CustomTextField!
+    @IBOutlet weak var detailsField: CustomTextField!
+    
+    var stores = [Store]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if let topItem = navigationController?.navigationBar.topItem {
             topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+        }
+        storePicker.delegate = self
+        storePicker.dataSource = self
+        
+//        let store1 = Store(context: context)
+//        store1.name = "Kubota Dealership"
+//        let store2 = Store(context: context)
+//        store2.name = "Apple Store"
+//        let store3 = Store(context: context)
+//        store3.name = "Ford Dealership"
+//        let store4 = Store(context: context)
+//        store4.name = "Amazon"
+//        let store5 = Store(context: context)
+//        store5.name = "Stihl Dealership"
+//        let store6 = Store(context: context)
+//        store6.name = "PJ Trailer Dealership"
+//        
+//        ad.saveContext()
+        getStores()
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let store = stores[row]
+        return store.name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stores.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+    
+    func getStores() {
+        let fetchRequest: NSFetchRequest<Store> = Store.fetchRequest()
+        do {
+            stores = try context.fetch(fetchRequest)
+            storePicker.reloadAllComponents()
+        } catch {
+            
         }
     }
 }
